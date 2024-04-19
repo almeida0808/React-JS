00458e727bd45bb5b549d6f4990f9d46f8cda2c8
almeida0808 <almeidaff08@gmail.com>
2024-04-18 21:35:10 -0300

    CSS in JS ⚡️temas, e estilizações globais.

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Details } from "./pages/Details";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "./styles/global"
+
+import temas from "./styles/temas";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={temas}>
+      <GlobalStyles/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 );
